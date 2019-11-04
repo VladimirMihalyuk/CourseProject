b430a5f5fa67a4f99cbf7809e27149612f44fc39
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2019-11-04 17:04:48 +0300

    Changed icon

--- app/src/main/java/com/example/courseproject/accounting/AccountigFragment.kt
+++ app/src/main/java/com/example/courseproject/accounting/AccountigFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.courseproject.App
 import com.example.courseproject.R
@@ -33,6 +34,7 @@ class AccountigFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_accountig, container, false)
         view.button2.setOnClickListener{
             findNavController().navigate(AccountigFragmentDirections.actionAccountigFragmentToLogInFlowActivity())
+
         }
 
         return view
