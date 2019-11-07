af34648964212ffd4e7bdc1101616d9673455f6c
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2019-11-07 23:17:52 +0300

    Added AccountingItemsListFragment implementation

--- app/src/main/java/com/example/courseproject/accounting/AccountingItemsListFragment.kt
+++ app/src/main/java/com/example/courseproject/accounting/AccountingItemsListFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 
 import com.example.courseproject.R
 import com.example.courseproject.repository.Repository
+import kotlinx.android.synthetic.main.fragment_accounting_items_list.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -31,7 +33,22 @@ class AccountingItemsListFragment : Fragment() {
 
         val item = AccountingItemsListFragmentArgs.fromBundle(arguments!!).itemInfo
 
-        if(item.)
+        val adapter = AdapterForListOfAccountingItems()
+        view.list.adapter = adapter
+
+        if(item.AccountingType ==1 ){
+            viewModel.incomeItemsMap.observe(this, Observer { map ->
+                map?.let{
+                   adapter.submitList(map.get(Triple(item.Type, item.Icon, item.IdOfICType)))
+                }
+            })
+        }else{
+            viewModel.costsItemsMap.observe(this, Observer { map ->
+                map?.let{
+                    adapter.submitList(map.get(Triple(item.Type, item.Icon, item.IdOfICType)))
+                }
+            })
+        }
 
 
 
